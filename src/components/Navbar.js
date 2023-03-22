76bde88d8d4b1ff05591846bb99835e6237a2c98
betroboomin <121827404+betroboomin@users.noreply.github.com>
2023-03-22 22:55:28 +0100

    nav bar complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,9 @@ import Logo from '../assets/images/Logo.png';
 const Navbar = () => {
   return (
     <Stack
-    paddingLeft="1rem"
+      direction="row"
+      justifyContent="space-around"
+      sx = {{ gap: {sm:'122px', xs:'40px'}, mt:{sm:'32px', xs:'20px'},justifyContent:'none', px:"20px"}}
     >
       <Link to="/">
         <img src={Logo} alt="Logo" style={{ width : '40px', height
@@ -15,7 +17,8 @@ const Navbar = () => {
       <Stack
       direction="row"
       gap="40px"
-      fontSize="25px"
+      fontSize="24px"
+      alignItems="flex-end"
       >
         <Link to="/" style={{ textDecoration :'none', color : '#3A1212', borderBottom : '3px solid  #FF2625'}}>Home</Link>
         <a href="#excercises" style={{ textDecoration :'none', color : '#3A1212' }}>Exercises</a>
